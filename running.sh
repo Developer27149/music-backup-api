db78f5e52497a44c41f35a49b85eeef8d4b0484a
youyiqin <564151947@qq.com>
2021-09-21 06:55:06 +0800

    调试

--- running.sh
+++ running.sh
@@ -1,6 +1,4 @@
 # 运行 pm2 服务
-# yarn && pm2 reload ecosystem.config.js && pm2 restart ecosystem.config.js
-cd ~
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh   
-yarn -h > sss
+yarn && pm2 reload ecosystem.config.js && pm2 restart ecosystem.config.js
